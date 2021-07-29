0f41aa754803b0813909c20074f2dc65725245d3
Drahaven <rochet.alexia@yahoo.fr>
2021-07-29 14:27:55 +0200

    Ajoutez des fichiers projet.

---
+++ Portfolio/Controllers/ExperiencesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Controllers
+{
+    public class ExperiencesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ExperiencesPro()
+        {
+            return View();
+        }
+
+        public IActionResult Formations()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
